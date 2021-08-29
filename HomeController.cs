62d579d12e9955afaf3654698555c708f84ab964
manak-3 <71026323+manak-3@users.noreply.github.com>
2021-08-29 16:54:03 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestASS2.Models;
+
+namespace TestASS2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        JoinedTables db = new JoinedTables();
+        public ActionResult Index()
+
+        {
+           
+            List<coutbl> c = db.coutbls.ToList();
+            return View(c);
+        }
+        public ActionResult studentinfo(int id)
+        {
+            List<Stutable> stu = db.Stutables.Where(s => s.course_id == id).ToList();
+            return View(stu);
+
+        }
+    }
+}
